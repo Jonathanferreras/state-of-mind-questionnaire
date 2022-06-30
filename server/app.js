04f7e08712fb4dc5029c77fd0e64a3a852e9df16
jonathanferreras <jonathanferreras@icloud.com>
2022-06-30 19:48:40 -0400

    init sqlite

--- server/app.js
+++ server/app.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const sqlite = require("sqlite3");
+const path = require("path");
+
 const PORT = process.env.PORT || 9000;
 const app = express();
+const dbPath = path.resolve(__dirname, "./db/app.db");
+const db = new sqlite.Database(dbPath);
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS SUBMISSIONS (
+      ID int NOT NULL PRIMARY KEY,
+      Name varchar(255) NOT NULL,
+      Dob DATE,
+      Age int,
+      Happiness int,
+      Energy int,
+      Hopefulness int,
+      HoursSlept int
+    );
+  `);
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
